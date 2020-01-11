6b8088bd094c3f22fd9a110238af059a12a4b2b8
JK.Jiang <jjkjjk333@sina.com>
2020-01-11 11:43:54 +0800

    feat: vgg16 constructor added

---
+++ trt_api_pytorch/vgg16_sample/build_network_with_trt_on_fp16.py
@@ -0,0 +1,32 @@
+import torch
+import tensorrt as trt
+from vgg16_397923af_trt import populate_network
+
+
+def build_engine(weights):
+    # flag implies the input batch is explicit. The input shape is {P * C * H * W}.
+    flag = 1 << int(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH)
+    with trt.Builder(TRT_LOGGER) as builder, builder.create_network(flag) as network:
+        # Populate the network using weights from the PyTorch model.
+        populate_network(network, weights)
+        builder.max_workspace_size = 4 * 1 << 30
+        if builder.platform_has_fast_fp16:
+            builder.fp16_mode = True
+            builder.strict_type_constraints = True
+            print("===> using fp16 mode")
+        config = builder.create_builder_config()
+        return builder.build_engine(network, config)
+
+
+vgg16_path = './vgg16-397923af.pth'
+vgg16_weights = torch.load(vgg16_path, map_location='cpu')
+TRT_LOGGER = trt.Logger(trt.Logger.VERBOSE)
+
+# Do inference with TensorRT
+with build_engine(vgg16_weights) as engine:
+    # Build an engine, allocate buffers and create a stream.
+    host_memory = engine.serialize()
+    output_engine = 'vgg16-397923af_fp16.engine'
+    print("===> Save %s\n" % output_engine)
+    with open(output_engine, "wb") as f:
+        f.write(engine.serialize())
